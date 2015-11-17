e1dc66886cb6f1211dd4cab7013eafb94e13b84f
mage.wang <wangm4a1@hotmail.com>
2015-11-17 09:04:04 +0800

    add todo

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-#
+﻿#
 # This file is part of The Principles of Modern Game AI.
 # Copyright (c) 2015, AiGameDev.com KG.
 #
 
+"""
+TODO 
+進漸式發展的劇情對話
+AI 偶而可以搶話?
+對話依現有條件而有所不同
+"""
 import vispy                    # Main application support.
 
 import window                   # Terminal input and display.
@@ -24,8 +30,6 @@ AGENT_RESPONSES = [
 ]
 
 class HAL9000(object):
-    
-    
     def __init__(self, terminal):
         self.chatbot = nltk.chat.Chat(AGENT_RESPONSES, nltk.chat.util.reflections)
         """Constructor for the agent, stores references to systems and initializes internal memory.
@@ -42,8 +46,9 @@ class HAL9000(object):
         elif self.first:
             self.terminal.log("Hello World! This is HAL.", align='right', color='#00805A')
             self.first = False
+
         else:
-            self.terminal.log(self.chatbot.respond(evt.text), align='right', color='#00805A')
+            self.terminal.log(self.respond(evt.text), align='right', color='#00805A')
 
     def on_command(self, evt):
         """Called when user types a command starting with `/` also done via events.
@@ -56,7 +61,12 @@ class HAL9000(object):
             self.terminal.log('\u2014 Now in the {}. \u2014'.format(evt.text[9:]), align='center', color='#404040')
             self.location = evt.text[9:]
 
+        elif evt.text.startswith('use'):
+            self.terminal.log('', align='center', color='#404040')
+            self.terminal.log('\u2014 Use {}. \u2014'.format(evt.text[4:]), align='center', color='#404040')
+            
         else:
+            
             self.terminal.log('Command `{}` unknown.'.format(evt.text), align='left', color='#ff3000')    
             self.terminal.log("I'm afraid I can't do that.", align='right', color='#00805A')
 
@@ -65,6 +75,8 @@ class HAL9000(object):
         """
         pass
 
+    def respond(self, str):
+        return self.chatbot.respond(str)
 
 class Application(object):
     
@@ -82,7 +94,8 @@ class Application(object):
         # Connect the terminal's existing events.
         self.window.events.user_input.connect(self.agent.on_input)
         self.window.events.user_command.connect(self.agent.on_command)
-
+        
+        
     def run(self):
         timer = vispy.app.Timer(interval=1.0)
         timer.connect(self.agent.update)
